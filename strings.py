ffe3d71181d8f4bb2d5433c2288e9afee69b3bc5
Bart Inglot <inglotbartosz@gmail.com>
2022-10-18 13:56:04 +1100

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,30 @@
+import sys, re, os
+
+n = 6
+ASCII_BYTE = rb" !\"#\$%&\'\(\)\*\+,-\./0123456789:;<=>\?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\[\]\^_`abcdefghijklmnopqrstuvwxyz\{\|\}\\\~\t"
+
+regexp = rb"([%s]{%d,})|((?:[%s]\x00){%d,}[%s])" % (ASCII_BYTE, n, ASCII_BYTE, (n-1), ASCII_BYTE)
+ascii_only_regexp = rb"([%s]{%d,})" % (ASCII_BYTE, n)
+
+pattern = re.compile(regexp)
+
+def process(stream):
+    data = stream.read()
+    for match in pattern.finditer(data):
+        yield match.group()
+
+def main(args):
+    if len(args) != 2:
+        print('Usage: %s <input_file>'  % os.path.basename(args[0]))
+        sys.exit(1)
+    with open(args[1], 'rb') as i_file:
+        for s in process(i_file):
+            decoded = ''
+            if re.match(ascii_only_regexp, s):
+                decoded = s.decode('ascii') 
+            else: 
+                decoded = (s+b'\00').decode('utf-16-le')
+            print(decoded)
+
+if __name__ == "__main__":
+    main(sys.argv)
